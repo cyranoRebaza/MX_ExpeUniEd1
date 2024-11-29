3e0806d3006015e8d9b5070767fc72cee651fcba
cyranoRebaza <144959092+cyranoRebaza@users.noreply.github.com>
2024-11-29 16:50:28 -0300

    FEAT(.cs): U1, introdocucion

---
+++ U1_introduccion/T02_segundoProyecto/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T02_segundoProyecto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola soy el segundo proyecto de consola..");
+            Console.ReadKey();
+        }
+    }
+}
