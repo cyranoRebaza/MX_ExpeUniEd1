4c8e77f70656a196a05e7746db8c333e26dac844
cyranoRebaza <144959092+cyranoRebaza@users.noreply.github.com>
2025-03-01 02:23:10 -0300

    FEAT(.cs): forms, ejemplo DateTime en consola

---
+++ U03_WindowsForms/WF03_DateTime/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WF03_DateTime
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declaro una variable fecha tipo DateTipe con el  constructor por defecto
+            DateTime fecha = new DateTime();
+
+            Console.WriteLine($"la fecha es: {fecha.ToString()}");
+
+            // declaro una variable fecha2 de tipo DateTipe con constructor con valores iniciales 
+            DateTime fecha2 = new DateTime(2004, 4, 1);
+            Console.WriteLine($"la fecha es: {fecha2.ToString()}");
+
+            // Declaro una variable sin llamar al constructor --> llamando DateTime.Now
+            DateTime fecha3 = DateTime.Now;
+            Console.WriteLine($"la fecha es: {fecha3.ToString()}");
+
+            // otros metodos para mostrar la fecha
+            Console.WriteLine($"la fecha es: {fecha3.AddDays(5)}"); // agrega 5 dias
+            Console.WriteLine($"la fecha es: {fecha3.ToShortDateString()}"); // fecha corta formato string
+            Console.WriteLine($"la fecha es: {fecha3.ToShortTimeString()}"); // hora corta formato string
+            Console.WriteLine($"la fecha es: {fecha3.ToString("dd/MM/yyyy")}"); // formato personalizado
+            Console.ReadKey();
+        }
+    }
+}
