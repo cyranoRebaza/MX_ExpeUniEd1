ad25b719fab82b5721b1690eb5b2bbb57d92ae1a
cyranoRebaza <144959092+cyranoRebaza@users.noreply.github.com>
2025-03-06 14:55:48 -0300

    FEAT(.cs): forms, exception FormartException DivideByZeroException

---
+++ U03_WindowsForms/WF05_Excepciones/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace WF05_Excepciones
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtUno = new System.Windows.Forms.TextBox();
+            this.txtDos = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtUno
+            // 
+            this.txtUno.Location = new System.Drawing.Point(173, 92);
+            this.txtUno.Name = "txtUno";
+            this.txtUno.Size = new System.Drawing.Size(100, 22);
+            this.txtUno.TabIndex = 0;
+            // 
+            // txtDos
+            // 
+            this.txtDos.Location = new System.Drawing.Point(173, 153);
+            this.txtDos.Name = "txtDos";
+            this.txtDos.Size = new System.Drawing.Size(100, 22);
+            this.txtDos.TabIndex = 1;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(183, 228);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 31);
+            this.btnCalcular.TabIndex = 2;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(192, 308);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(17, 16);
+            this.lblResultado.TabIndex = 3;
+            this.lblResultado.Text = "= ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(441, 370);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtDos);
+            this.Controls.Add(this.txtUno);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUno;
+        private System.Windows.Forms.TextBox txtDos;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label lblResultado;
+    }
+}
+
---
+++ U03_WindowsForms/WF05_Excepciones/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WF05_Excepciones
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            int num1, num2, resultado;
+
+            try
+            {
+                num1 = int.Parse(txtUno.Text);
+                num2 = int.Parse(txtDos.Text);
+
+                resultado = num1 / num2;
+
+                lblResultado.Text = $"= {resultado}";
+            }
+            catch (FormatException ex)
+            {
+
+                MessageBox.Show("Por favor, cargar solo números...");
+            }
+            catch (DivideByZeroException ex)
+            {
+                MessageBox.Show("No se puede dividir por cero...");
+
+            }
+        }
+    }
+}
